dc512ec759bafafce51036f16023be7536ddafcc
TengoMarquez <1582655161@qq.com>
2025-10-16 16:17:46 +0800

    Add testSubtraction (will fail)

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -10,5 +10,9 @@ public class CalcTest {
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
-
+    @Test
+    void testSubtraction() {
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4, 2)); // 预期 4 - 2 = 2，但实际若 subtract 有问题，测试会失败
+    }
 }
